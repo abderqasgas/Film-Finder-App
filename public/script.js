baa6d8775a8f55464695c5736b72d192e01e6853
Abdo <abderqasgas@gmail.com>
2026-03-22 18:56:44 -0500

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,40 @@
+const tmdbKey = '3f569a3a8ab05701e4a56125e11daede';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const playBtn = document.getElementById('playBtn');
+
+const getGenres = async () => {
+    const genreRequestEndpoint = '/genre/movie/list';
+    const requestParams = `?apikey=${tmdbKey}`;
+    const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+
+    try {
+        const response = await fetch(urlToFetch);
+        if(response.ok){
+            const jsonResponse = response.json();
+        }
+    } 
+    catch (error) {
+        console.log(error);
+    }
+};
+
+const getMovies = () => {
+  const selectedGenre = getSelectedGenre();
+
+};
+
+const getMovieInfo = () => {
+
+};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
