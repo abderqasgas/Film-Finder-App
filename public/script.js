c3b1e8f14b25da80942799f7325b3302e539af8b
Abdo <abderqasgas@gmail.com>
2026-03-22 21:11:27 -0500

    genre functionality

--- public/script.js
+++ public/script.js
@@ -4,16 +4,19 @@ const playBtn = document.getElementById('playBtn');
 
 const getGenres = async () => {
     const genreRequestEndpoint = '/genre/movie/list';
-    const requestParams = `?apikey=${tmdbKey}`;
+    const requestParams = `?api_key=${tmdbKey}`;
     const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+    
 
     try {
         const response = await fetch(urlToFetch);
         if(response.ok){
-            const jsonResponse = response.json();
+            // console.log('i am working')
+            const jsonResponse = await response.json();
             // console.log(jsonResponse);
             const genres = jsonResponse.genres;
-            console.log(genres);
+            // console.log(genres);
+            return genres;
         }
     } 
     catch (error) {
@@ -21,13 +24,44 @@ const getGenres = async () => {
     }
 };
 
-const getMovies = () => {
+const getMovies = async () => {
   const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = '/discover/movie';
+  const requestParams = `?api_key=${tmdbKey}&with_genres${selectedGenre}`;
+  const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+  
+  try {
+    const response = await fetch(urlToFetch);
 
+    if(response.ok){
+        const jsonResponse = await response.json();
+        // console.log(jsonResponse);
+        const movies = jsonResponse.results;
+        // console.log(movies);
+    }
+  } 
+  catch (error) {
+    console.log(error);
+  }
 };
 
-const getMovieInfo = () => {
 
+const getMovieInfo = async (movie) => {
+    const movieId = movie.id
+    const movieEndpoint = `/movie/${movieId}`;
+    const requestParams = `?api_key=${tmdbKey}`;
+    const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+
+    try {
+        const response = await fetch(urlToFetch);
+        if(response.ok){
+            const movieInfo = await response.json();
+            return movieInfo;
+        }
+    } 
+    catch (error) {
+        console.log(error);    
+    }
 };
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
